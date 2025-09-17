fa63d843a2c4a4129964051d19dc7296363b10a7
saravargas88 <128425335+saravargas88@users.noreply.github.com>
2025-09-16 21:34:21 -0400

    Update home.tsx

--- sarawebsite/src/components/Home/home.tsx
+++ sarawebsite/src/components/Home/home.tsx
@@ -97,7 +97,7 @@ export default function Home() {
 
   {/* Headshot Side */}
   <motion.img
-    src="/headshot.jpg" 
+    src="/headshot.JPG" 
     alt="Sara Vargas Headshot"
     initial={{ opacity: 0, scale: 0.9 }}
     whileInView={{ opacity: 1, scale: 1 }}
