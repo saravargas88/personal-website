7df033a3898333c2aaf7db1a63af2916cafbe8dd
saravargas88 <128425335+saravargas88@users.noreply.github.com>
2026-01-28 13:21:59 -0500

    Update main.tsx

--- sarawebsite/src/main.tsx
+++ sarawebsite/src/main.tsx
@@ -5,8 +5,12 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+//BY WRAPPING THE ROOT COMPONENT IN A STRIC MODE WE ARE ADDING CHECKS
+// FOR POTENTIAL PROBLEMS IN THE APP (CHECKS ONLY RUN IN DEVELOPMENT)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
